3e87518607012171bfab55756ce71330a42c1a65
Masatoshi Teruya <mah0x211@gmail.com>
2016-03-11 16:52:26 +0900

    Add a connect function

--- src/lmqttpacket.c
+++ src/lmqttpacket.c
@@ -8,9 +8,41 @@
 #include <MQTTPacket.h>
 
 
+static int connect_lua( lua_State *L )
+{
+    unsigned char *buf = NULL;
+    int buflen = 0;
+    // MQTTPacket_connectData at MQTTConnect.h
+    MQTTPacket_connectData opts = MQTTPacket_connectData_initializer;
+
+    // alloc buffer
+    buflen = MQTTPacket_len( MQTTSerialize_connectLength( &opts ) );
+    if( ( buf = malloc( buflen ) ) )
+    {
+        // create connect packet
+        int len = MQTTSerialize_connect( buf, buflen, &opts );
+
+        if( len > 0 ){
+            lua_pushlstring( L, buf, len );
+            free( buf );
+            return 1;
+        }
+
+        free( buf );
+    }
+
+    // got error
+    lua_pushnil( L );
+    lua_pushstring( L, strerror( errno ) );
+
+    return 2;
+}
+
+
 LUALIB_API int luaopen_mqttpacket( lua_State *L )
 {
     struct luaL_Reg funcs[] = {
+        { "connect", connect_lua },
         { NULL, NULL }
     };
     struct luaL_Reg *ptr = funcs;
