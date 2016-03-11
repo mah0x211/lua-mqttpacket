fde96bf2d9f36bc3a49910f1aa58821382b44dcb
Masatoshi Teruya <mah0x211@gmail.com>
2016-03-11 17:28:29 +0900

    Add a publish function

--- src/lmqttpacket.c
+++ src/lmqttpacket.c
@@ -8,6 +8,55 @@
 #include <MQTTPacket.h>
 
 
+
+static int publish_lua( lua_State *L )
+{
+    MQTTString topic = MQTTString_initializer;
+    size_t tlen = 0;
+    size_t plen = 0;
+    const char *payload = NULL;
+    unsigned char *buf = NULL;
+
+    // check arguments
+    // topic
+    topic.lenstring.data = lauxh_checklstring( L, 1, &tlen );
+    topic.lenstring.len = tlen;
+    // payload
+    payload = lauxh_checklstring( L, 2, &plen );
+
+    // topic/payload too large
+    if( tlen > INT_MAX || plen > INT_MAX ){
+        errno = EOVERFLOW;
+    }
+    else
+    {
+        int len = 0;
+        int buflen = MQTTPacket_len(
+            MQTTSerialize_publishLength( 0, topic, plen )
+        );
+
+        if( ( buf = malloc( buflen ) ) )
+        {
+            len = MQTTSerialize_publish( buf, buflen, 0/*dup*/, 0/*qos*/,
+                                         0/*retained*/, 0/*packetid*/, topic,
+                                         payload, plen );
+            if( len > 0 ){
+                lua_pushlstring( L, buf, len );
+                free( buf );
+                return 1;
+            }
+            free( buf );
+        }
+    }
+
+    // got error
+    lua_pushnil( L );
+    lua_pushstring( L, strerror( errno ) );
+
+    return 2;
+}
+
+
 static int disconnect_lua( lua_State *L )
 {
     unsigned char *buf[3] = { 0 };
@@ -63,6 +112,7 @@ LUALIB_API int luaopen_mqttpacket( lua_State *L )
     struct luaL_Reg funcs[] = {
         { "connect", connect_lua },
         { "disconnect", disconnect_lua },
+        { "publish", publish_lua },
         { NULL, NULL }
     };
     struct luaL_Reg *ptr = funcs;
