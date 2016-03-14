badb057a7337d1264fd70a528462e3b549e3a769
Masatoshi Teruya <mah0x211@gmail.com>
2016-03-14 22:15:57 +0900

    Add a subscribes function

--- src/client.c
+++ src/client.c
@@ -126,42 +126,109 @@ static int unsubscribe_lua( lua_State *L )
 }
 
 
-static int subscribe_lua( lua_State *L )
+static int subscribes_lua( lua_State *L )
 {
-    MQTTString topic[] = { MQTTString_initializer };
-    int qos[] = {0};
+    lua_State *th = NULL;
     size_t tlen = 0;
-    unsigned char *buf = NULL;
+    unsigned short id = 0;
+    unsigned char dup = 0;
+    lua_Integer v = 0;
 
-    // check arguments
-    // topic
-    topic[0].lenstring.data = (char*)lauxh_checklstring( L, 1, &tlen );
-    topic[0].lenstring.len = tlen;
+    lua_settop( L, 1 );
+    // check type
+    lauxh_checktable( L, 1 );
 
-    // topic length too large
-    if( tlen > INT_MAX ){
-        errno = EOVERFLOW;
-    }
-    else
+    // id
+    v = lauxh_optintegerof( L, 1, "id", 0 );
+    lauxh_argcheck(
+        L, v >= 0 && v <= UINT16_MAX, 1, "id must be up to UINT16_MAX"
+    );
+    id = v;
+
+    // dup flag
+    dup = (unsigned char)lauxh_optbooleanof( L, 1, "dup", dup );
+
+    // check topic type
+    lauxh_gettblof( L, "topic", 1 );
+    lauxh_argcheck( L, lauxh_istable( L, -1 ), 1, "topic must be table" );
+
+    // create thread for container
+    th = lua_newthread( L );
+    if( th )
     {
-        int len = 0;
-        int buflen = MQTTPacket_len(
-            MQTTSerialize_subscribeLength( 1/*count*/, topic )
-        );
+        size_t ntopic = 0;
+        const char *str = NULL;
+        MQTTString *topic = NULL;
+        int *qos = NULL;
 
-        if( ( buf = malloc( buflen ) ) )
+        // move thread to the stack bottom
+        lua_replace( L, 1 );
+        // check topic table by loop
+        lua_pushnil( L );
+        while( lua_next( L, -2 ) ){
+            // topic
+            lauxh_checklstring( L, -2, &tlen );
+            lauxh_argcheck(
+                L, tlen >= 0 && v <= UINT16_MAX, 1,
+                "topic length must be up to UINT16_MAX"
+            );
+
+            // qos
+            v = lauxh_checkinteger( L, -1 );
+            // invalid qos value range
+            lauxh_argcheck(
+                L, v >= 0 && v <= 2, 2,
+                "qos must be range of 0 to 2"
+            );
+
+            // copy topic to stack top
+            lua_pushvalue( L, -2 );
+            // move qos and topic into thread stack
+            lua_xmove( L, th, 2 );
+
+            ntopic++;
+        }
+        lua_pop( L, 1 );
+
+        // create topic and qos arrays
+        topic = lua_newuserdata( L, sizeof( MQTTString ) * ntopic );
+        if( topic && ( qos = lua_newuserdata( L, sizeof( int ) * ntopic ) ) )
         {
-            len = MQTTSerialize_subscribe( buf, buflen, 0/*dup*/, 0/*packetid*/,
-                                           1/*count*/, topic, qos );
+            size_t i = 0;
+            unsigned char *buf = NULL;
+            int buflen = 0;
 
-            if( len > 0 ){
-                lua_pushlstring( L, (const char*)buf, len );
-                free( buf );
-                return 1;
+            for(; i < ntopic; i++ ){
+                // topic
+                topic[i].cstring = NULL;
+                topic[i].lenstring.data = (char*)lua_tolstring(
+                    th, -1, &topic[i].lenstring.len
+                );
+
+                // qos
+                qos[i] = lua_tointeger( th, -2 );
+                lua_pop( th, 2 );
             }
 
-            errno = ENOBUFS;
-            free( buf );
+            // create buffer
+            buflen = MQTTPacket_len(
+                MQTTSerialize_subscribeLength( ntopic, topic )
+            );
+            if( ( buf = malloc( buflen ) ) )
+            {
+                int len = MQTTSerialize_subscribe( buf, buflen, dup, id, ntopic,
+                                                   topic, qos );
+
+                if( len > 0 ){
+                    lua_settop( L, 0 );
+                    lua_pushlstring( L, (const char*)buf, len );
+                    free( buf );
+                    return 1;
+                }
+
+                errno = ENOBUFS;
+                free( buf );
+            }
         }
     }
 
@@ -170,6 +237,61 @@ static int subscribe_lua( lua_State *L )
     lua_pushstring( L, strerror( errno ) );
 
     return 2;
+}
+
+
+
+static int subscribe_lua( lua_State *L )
+{
+    size_t tlen = 0;
+    const char *topic = lauxh_checklstring( L, 1, &tlen );
+
+    // invalid topic length
+    lauxh_argcheck(
+        L, tlen > 0 && tlen <= UINT16_MAX, 1,
+        "topic length must be up to UINT16_MAX"
+    );
+
+    // option
+    if( lua_gettop( L ) > 1 )
+    {
+        lua_Integer qos = 0;
+
+        lua_settop( L, 2 );
+        lauxh_checktable( L, 2 );
+
+        // qos
+        qos = lauxh_optintegerof( L, 2, "qos", 0 );
+        // invalid qos value range
+        lauxh_argcheck(
+            L, qos >= 0 && qos <= 2, 2, "qos must be range of 0 to 2"
+        );
+
+        // remove qos field
+        lauxh_pushnil2tbl( L, "qos" );
+
+        // append topic table
+        lua_pushliteral( L, "topic" );
+        lua_createtable( L, 0, 1 );
+        lauxh_pushint2tbl( L, topic, qos );
+        lua_rawset( L, -3 );
+        // replace first argument
+        lua_replace( L, 1 );
+
+        return subscribes_lua( L );
+    }
+
+    // create table
+    lua_createtable( L, 0, 1 );
+    lua_pushliteral( L, "topic" );
+    lua_createtable( L, 0, 1 );
+    lauxh_pushint2tbl( L, topic, 0 );
+    lua_rawset( L, -3 );
+
+    // replace first argument
+    lua_replace( L, 1 );
+
+    return subscribes_lua( L );
 }
 
 
@@ -207,15 +329,18 @@ static int publish_lua( lua_State *L )
         lua_Integer v = 0;
 
         lua_settop( L, 3 );
-        lauxh_checktable( L, -1 );
+        lauxh_checktable( L, 3 );
+        // dup
         dup = lauxh_optbooleanof( L, 3, "dup", 0 );
         retain = lauxh_optbooleanof( L, 3, "retain", 0 );
 
+        // qos
         v = lauxh_optintegerof( L, 3, "qos", 0 );
         // invalid qos value range
         lauxh_argcheck( L, v >= 0 && v <= 2, 3, "qos must be range of 0 to 2" );
         qos = v;
 
+        // id
         v = lauxh_optintegerof( L, 3, "id", 0 );
         // invalid qos value range
         lauxh_argcheck(
@@ -306,6 +431,7 @@ LUALIB_API int luaopen_mqttpacket_client( lua_State *L )
         { "disconnect", disconnect_lua },
         { "publish", publish_lua },
         { "subscribe", subscribe_lua },
+        { "subscribes", subscribes_lua },
         { "unsubscribe", unsubscribe_lua },
         { NULL, NULL }
     };
